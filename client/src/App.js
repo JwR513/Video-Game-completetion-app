98a7b5095794a11c5cfba10ce92b8f79021ee0e5
Johnathan Weil <weilj872@gmail.com>
2022-07-25 12:13:12 -0400

    router stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import React from 'react'
+import router from '../../routes'
 import './App.css'
+import { Routes, route } from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Routes>
+        <Route />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
