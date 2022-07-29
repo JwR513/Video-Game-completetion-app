40726dd2154bba0cf56deec4ac140610b029a22c
Johnathan Weil <weilj872@gmail.com>
2022-07-29 10:50:59 -0400

    herokudeploy

--- db/index.js
+++ db/index.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/GamesDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/GamesDatabase'
 
 mongoose
-  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
