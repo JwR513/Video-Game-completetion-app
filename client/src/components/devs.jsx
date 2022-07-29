9e302c852bb0a8067b2115b7d43b3db7008f2be9
Johnathan Weil <weilj872@gmail.com>
2022-07-29 09:46:56 -0400

    dev styling added

--- client/src/components/devs.jsx
+++ client/src/components/devs.jsx
@@ -2,7 +2,7 @@ export default function Devs(props){
   
     return(
   <div>
-      <section>
+      <section className='single-dev'>
         <h3>Studio: {props.dev.studioName}</h3>
         <h3>Ceo: {props.dev.ceoName}</h3>
       </section>
