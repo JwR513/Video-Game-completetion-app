f43be123ddda9a4b3a2fe4c24736a89ec0528d03
Johnathan Weil <weilj872@gmail.com>
2022-07-29 10:40:34 -0400

    server.js heroku stuff added

--- server.js
+++ server.js
@@ -13,11 +13,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(logger('dev'))
+app.use(express.static(`${__dirname}/client/build`))
 
 // app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
